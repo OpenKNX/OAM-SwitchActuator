1ba076e604340b2300eb5aa09171671f39198c22
Ing-Dom <dom@ing-dom.de>
2024-08-12 10:12:43 +0200

    test oam for SA-REG1-4xSELV

--- include/versions.h
+++ include/versions.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#define MAIN_Version "b73a5cf"
-#define KNX_Version "acf4a0b"
+#define MAIN_Version "340fa62"
+#define KNX_Version "2.1.0+845aa38"
 #define MODULE_LogicModule_Version "3.3.0+9f85150"
 #define MODULE_LogicModule_ETS 51
-#define MODULE_Common_Version "1.2.0+73e09e3"
+#define MODULE_Common_Version "1.2.0+f1367ca"
 #define MODULE_Common_ETS 18
-#define MODULE_SwitchActuator_Version "0.1.0+19138d9"
+#define MODULE_SwitchActuator_Version "0.1.0+0201603"
 #define MODULE_SwitchActuator_ETS 1
+#define MODULE_GPIOModule_Version "0.0.0+da78550"
+#define MODULE_GPIOModule_ETS 0
 #define MODULE_FileTransferModule_Version "0.0.4+db4a240"
 #define MODULE_FileTransferModule_ETS 0
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Logic.h"
 #include "SwitchActuatorModule.h"
+#include "GPIOModule.h"
 #include "FileTransferModule.h"
 #include "OpenKNX.h"
 #include "hardware.h"
@@ -9,13 +10,20 @@ void setup()
     const uint8_t firmwareRevision = 0;
 
     openknx.init(firmwareRevision);
-    openknx.addModule(1, openknxLogic);
-    openknx.addModule(2, openknxSwitchActuatorModule);
-    openknx.addModule(3, openknxFileTransferModule);
+    //openknx.addModule(1, openknxLogic);
+    openknx.addModule(4, openknxGPIOModule); // before any using modules !
+    //openknx.addModule(2, openknxSwitchActuatorModule);
+    //openknx.addModule(3, openknxFileTransferModule);
+    
     openknx.setup();
+    openknxGPIOModule.pinMode(24, OUTPUT);
 }
 
 void loop()
 {
     openknx.loop();
+    openknxGPIOModule.digitalWrite(24, HIGH);
+    delay(200);
+    openknxGPIOModule.digitalWrite(24, LOW);
+    delay(1000);
 }
