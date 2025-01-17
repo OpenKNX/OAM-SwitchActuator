a9a229c23146e913f3d52367b76805a1aed753b6
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-01-17 18:39:18 +0100

    Use OpenKNX GPIO naming for I2C.

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "e8b863c"
+#define MAIN_Version "451ba2b"
 #define KNX_Version "2.1.2+764900e"
 #define MODULE_LogicModule_Version "3.5.2+9a1cfe1"
 #define MODULE_LogicModule_Version_Major 3
@@ -12,7 +12,7 @@
 #define MODULE_Common_Version_Minor 2
 #define MODULE_Common_Version_Revision 0
 #define MODULE_Common_ETS 18
-#define MODULE_SwitchActuator_Version "0.1.0+1b67972"
+#define MODULE_SwitchActuator_Version "0.1.0+cc3d751"
 #define MODULE_SwitchActuator_Version_Major 0
 #define MODULE_SwitchActuator_Version_Minor 1
 #define MODULE_SwitchActuator_Version_Revision 0
