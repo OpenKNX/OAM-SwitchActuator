a67250769c752a2eac92c0004d1291cf4769b535
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-01-17 18:24:17 +0100

    Updated for OpenKNX library changes.

--- include/versions.h
+++ include/versions.h
@@ -1,12 +1,24 @@
 #pragma once
 
-#define MAIN_Version "941142d"
-#define KNX_Version "2.1.0+84a0ce3"
-#define MODULE_LogicModule_Version "3.3.1+9335c33"
-#define MODULE_LogicModule_ETS 51
-#define MODULE_Common_Version "1.2.0+8410b2a"
+#define MAIN_Version "e8b863c"
+#define KNX_Version "2.1.2+764900e"
+#define MODULE_LogicModule_Version "3.5.2+9a1cfe1"
+#define MODULE_LogicModule_Version_Major 3
+#define MODULE_LogicModule_Version_Minor 5
+#define MODULE_LogicModule_Version_Revision 2
+#define MODULE_LogicModule_ETS 53
+#define MODULE_Common_Version "1.2.0+6f467fb"
+#define MODULE_Common_Version_Major 1
+#define MODULE_Common_Version_Minor 2
+#define MODULE_Common_Version_Revision 0
 #define MODULE_Common_ETS 18
-#define MODULE_SwitchActuator_Version "0.1.0+cfaf665"
+#define MODULE_SwitchActuator_Version "0.1.0+1b67972"
+#define MODULE_SwitchActuator_Version_Major 0
+#define MODULE_SwitchActuator_Version_Minor 1
+#define MODULE_SwitchActuator_Version_Revision 0
 #define MODULE_SwitchActuator_ETS 1
-#define MODULE_FileTransferModule_Version "0.0.4+db4a240"
-#define MODULE_FileTransferModule_ETS 0
+#define MODULE_FileTransferModule_Version "0.1.0+2669301"
+#define MODULE_FileTransferModule_Version_Major 0
+#define MODULE_FileTransferModule_Version_Minor 1
+#define MODULE_FileTransferModule_Version_Revision 0
+#define MODULE_FileTransferModule_ETS 1
