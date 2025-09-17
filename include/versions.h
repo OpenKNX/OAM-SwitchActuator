096ff9f0e303f484b9972f593f8f22146afb3cbc
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-09-17 19:48:33 +0200

    Working on current sense variant.

--- include/versions.h
+++ include/versions.h
@@ -1,18 +1,18 @@
 #pragma once
 
-#define MAIN_Version "58ee6fe"
+#define MAIN_Version "878b011"
 #define KNX_Version "2.2.1+1b646c1"
 #define MODULE_LogicModule_Version "3.7.1+1ba35cb"
 #define MODULE_LogicModule_Version_Major 3
 #define MODULE_LogicModule_Version_Minor 7
 #define MODULE_LogicModule_Version_Revision 1
 #define MODULE_LogicModule_ETS 55
-#define MODULE_Common_Version "1.4.3+2f8171e"
+#define MODULE_Common_Version "1.4.3+24fc5b5"
 #define MODULE_Common_Version_Major 1
 #define MODULE_Common_Version_Minor 4
 #define MODULE_Common_Version_Revision 3
 #define MODULE_Common_ETS 20
-#define MODULE_SwitchActuator_Version "0.1.0+c35288a"
+#define MODULE_SwitchActuator_Version "0.1.0+f763577"
 #define MODULE_SwitchActuator_Version_Major 0
 #define MODULE_SwitchActuator_Version_Minor 1
 #define MODULE_SwitchActuator_Version_Revision 0
